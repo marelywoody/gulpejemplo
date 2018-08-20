122a31af9debc276991d12cc47ff98e8bd9f8130
marelywoody <ros_ely_arc@hotmail.com>
2018-08-20 17:34:41 -0500

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+// compilar archivos scss a css
+gulp.task('sass',function(){
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'])
+  .pipe(sass())
+  .pipe(gulp.dest('src/css'))
+  .pipe(browserSync.stream());
+});
+
+// compilar archivos scss a js
+gulp.task('js',function(){
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.js','node_modules/jquery/dist/jquery.js','node_modules/tether/dist/js/tether.js'])
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.stream());
+  });
+
+gulp.task('serve',['sass'],function(){
+  browserSync.init({
+      server:'./src'
+  });
+  gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'],['sass']);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
+});  
